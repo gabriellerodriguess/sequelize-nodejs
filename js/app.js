27f37aaff1f0db7ba3456d060f3e628cb091ef7b
Gabrielle Rodrigues <gabriellerut@gmail.com>
2023-02-06 08:16:26 -0300

    feat/001: template engine and bodyParser configuration for sending form by post method

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
 import express from "express" // const express = require('express')
-import { engine } from 'express-handlebars'
 import { Sequelize } from 'sequelize'
-
+import { create } from 'express-handlebars';
+import bodyParser from 'body-parser'
 
 const app = express() //cria uma instância (cópia) do framework express
+const hbs = create({})
+
+// Register `hbs.engine` with the Express app.
 
 //Configs
     //Config template engine Handlebars
-        app.engine('handlebars', engine({defaultLayout: 'main'}))
-        app.set('view engine', 'handlebars')
+    app.engine('handlebars', hbs.engine);
+    app.set('view engine', 'handlebars');
+    app.set('views', '../views');
+
+    //Body Parser
+    app.use(bodyParser.urlencoded({extended: false}))
+    app.use(bodyParser.json())
+
     //Database MYSQL
         const sequelize = new Sequelize('test_sequelize', 'root', 'mysql123', {
             host: 'localhost', //qual servidor está o banco de dados
@@ -17,7 +26,11 @@ const app = express() //cria uma instância (cópia) do framework express
 //
 
 app.get('/cadastro', (req,res) => {
-    res.send('Rota para o cadastro de posts')
+    res.render('form')
+})
+
+app.post('/insert', (req,res) => { 
+    res.send(`Title:${req.body.title} / Content: ${req.body.content}`)
 })
 
 app.listen(3030, () => {
